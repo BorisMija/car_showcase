1aea0c7f3720ff1f62f4f559b61f13cda69a896d
BorisMija <boris.mija@isa.utm.md>
2025-09-03 19:36:10 +0300

    Project upload

---
+++ app/page.tsx
@@ -0,0 +1,50 @@
+import { CustomFilter, Hero, SearchBar } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
+import { fetchCars } from "@/utils";
+import CarCard from "@/components/CarCard";
+
+export default async function Home() {
+  const allCars = await fetchCars({ // trebuie apelată cu parametri chiar și default
+    manufacturer: "",
+    year: 2022,
+    fuel: "",
+    limit: 10,
+    model: "",
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+
+      <div className="mt-10 padding-x padding-y max-width" id="discover">
+        <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+        <p>Explore the cars you might like</p>
+
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car, index) => (
+                <CarCard key={index} car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
---
+++ types/index.ts
@@ -0,0 +1,32 @@
+"use client";
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>; // ✅ Corectat numele
+  btnType?: "button" | "submit";
+  rightIcon?: string;
+  isDisabled?: boolean;
+  textStyles?: string; // ✅ Adăugat
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
