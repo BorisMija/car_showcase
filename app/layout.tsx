87f08ce1e3c824a94cc25ec97e449494f1dd93b6
BorisMija <boris.mija@isa.utm.md>
2025-09-04 16:22:05 +0300

    Second day

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar, Footer } from "@/components";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { Footer, Navbar } from "@/components";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "Car Hub",
-  description: "Discover the best cars in the world",
+  description: "Discover world's best car showcase application",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body className="relative">
+    <html lang='en'>
+      <body className='relative'>
         <Navbar />
         {children}
         <Footer />
--- types/index.ts
+++ types/index.ts
@@ -30,3 +30,31 @@ export interface CarProps {
   transmission: string;
   year: number;
 }
+export interface FilterProps{
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
+}
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+    export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
+export interface HomeProps {
+  searchParams: {
+    manufacturer?: string;
+    year?: number;
+    fuel?: string;
+    limit?: number;
+    model?: string;
+  };
+}
